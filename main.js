ca68e4293051f0635b61e223b7d4aa5046255469
Othmane haiba <othmanehaiba4@gmail.com>
2025-10-29 12:08:43 +0100

    i added i file called help to type what i need

--- main.js
+++ main.js
@@ -1,8 +1,2 @@
-const container = document.getElementById('grille');
-for (let i = 1; i <= 25; i++) {
-  const d = document.createElement('div');
-  d.className = 'Day';
-  d.textContent = `${i}`;
-  d.dataset.index = i; // useful metadata
-  container.appendChild(d);
-}
+
+
