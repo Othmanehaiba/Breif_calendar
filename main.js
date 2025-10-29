7359a2a77209b16020cc828897d7b0588e3ada13
Othmane haiba <othmanehaiba4@gmail.com>
2025-10-29 12:26:02 +0100

    i added a form

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
+const day = document.querySelectorAll(".day")
+
+const popUp = document.createElement("div")
+const form = document.createElement("form")
+const label = document.createElement("label")
+const input = document.createElement("input")
+label.textContent = 'Name'
+document.body.append(form)
+form.append(label)
+form.append(input)
 
 
