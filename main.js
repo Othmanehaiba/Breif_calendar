e914bf49022d994b91627b49d3bf03f2d2c8ba52
Othmane haiba <othmanehaiba4@gmail.com>
2025-10-29 16:46:48 +0100

    i fixed some issues in JavaScript

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-const day = document.querySelectorAll(".day")
+const days = document.querySelector(".days");
 
-const popUp = document.createElement("div")
-const form = document.createElement("form")
-const input = document.createElement("input")
-label.textContent = 'Name'
-document.body.appendChild(form)
-form.appendChild(input)
-input.setAttribute("type" , "text")
-form.style.display = "none"
+const popUp = document.querySelector(".pop_up")
+// const form = document.createElement("form");
+// const input = document.createElement("input");
+// label.textContent = "Name";
+// document.body.appendChild(form);
+// form.appendChild(input);
+// input.setAttribute("type", "text");
+// form.style.display = "none";
 
+// days.addEventListener("click", () => {
+//   popUp.style.display = 'block'
+// });
+
+form.forEach(element =>{
+  element.addEventListener('click', () => {
+    formul.style.display = 'block'; 
+  });
+});
