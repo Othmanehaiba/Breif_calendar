0743a3360e459570e604736814e95d300b7a7504
Othmane haiba <othmanehaiba4@gmail.com>
2025-10-29 16:04:40 +0100

    i added a continer

--- main.js
+++ main.js
@@ -2,11 +2,10 @@ const day = document.querySelectorAll(".day")
 
 const popUp = document.createElement("div")
 const form = document.createElement("form")
-const label = document.createElement("label")
 const input = document.createElement("input")
 label.textContent = 'Name'
-document.body.append(form)
-form.append(label)
-form.append(input)
-
+document.body.appendChild(form)
+form.appendChild(input)
+input.setAttribute("type" , "text")
+form.style.display = "none"
 
