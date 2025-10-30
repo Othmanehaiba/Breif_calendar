5d59d6486eea872c10d40fc654d31dc14ac1ed41
Othmane haiba <othmanehaiba4@gmail.com>
2025-10-30 15:00:19 +0100

    i have a problem in the js file

--- main.js
+++ main.js
@@ -1,22 +1,19 @@
 const days_name = document.querySelectorAll(".days_name");
-const popUp = document.querySelector('.pop_up');
+const popUp = document.querySelector(".pop_up");
 const day = document.querySelector(".milieu_calendar");
 
+// const name = document.getElementById(name)
 
-// const form = document.createElement("form");
-// const input = document.createElement("input");
-// label.textContent = "Name";
-// document.body.appendChild(form);
-// form.appendChild(input);
-// input.setAttribute("type", "text");
-// form.style.display = "none";
-// for(let i = 0 ; i < day.length() ; i++){
-//   day.addEventListener("click", () => {
-//   popUp.classList.toggle("show");
-// });
-// }
+const weekend = document.querySelector("#d6");
 
-day.addEventListener('click', () => {
-  popUp.style.display = 'block';  
+weekend.addEventListener("click", () => {
+  popUp
 });
+
+day.addEventListener("click", () => {
+  popUp.style.display = "block";
+});
+
+
+
 
