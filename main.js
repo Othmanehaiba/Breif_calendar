0fe27907e98dabca37cb133e05ed01da7c3776a2
Othmane haiba <othmanehaiba4@gmail.com>
2025-10-31 17:14:12 +0100

    i added somthing

--- main.js
+++ main.js
@@ -7,41 +7,36 @@ const popUp = document.querySelector(".pop_up");
 let cancel = document.getElementById("cancel");
 let curr_day = null;
 
-
 popUp.style.display = "none";
-
 
 let days = document.querySelectorAll(".days");
 
-// When user clicks a day, show popup and save clicked day
-days.forEach(day => {
+days.forEach((day) => {
   day.addEventListener("click", () => {
     popUp.style.display = "block";
-    curr_day = day; // store the clicked day
+    curr_day = day;
   });
 });
 
-// When user clicks "Annuler"
 cancel.addEventListener("click", function () {
   popUp.style.display = "none";
-});
+}); 
 
-// When form is submitted
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   let name = document.getElementById("name").value;
-  if (!curr_day) return; // safety check
+  if (!curr_day) return;
+  else{
+    curr_day = null;
+  }
 
-  // Create a div for reservation info
   let afficher = document.createElement("div");
   afficher.classList.add("affichage");
-  afficher.textContent = name; // add name text
+  afficher.textContent = name;
 
-  // Append inside the clicked day
   curr_day.appendChild(afficher);
 
-  // Reset and close popup
   form.reset();
   popUp.style.display = "none";
 });
