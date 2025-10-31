19f6633ca8886a22d0e7ceaf428994e5bffcab7a
Othmane haiba <othmanehaiba4@gmail.com>
2025-10-31 10:34:10 +0100

    added some new things

--- main.js
+++ main.js
@@ -1,25 +1,45 @@
+let heureD = document.getElementById("heureDebut");
+let heureF = document.getElementById("heureFin");
+let nbrPersonne = document.getElementById("nbrPersonne");
+let typeReservation = document.getElementById("typeReservation");
+let form = document.querySelector(".formulaire");
+let hide = document.getElementById("hidden")
+let submit = document.getElementById("submit");
 const days_name = document.querySelectorAll(".days_name");
 const popUp = document.querySelector(".pop_up");
 const day = document.querySelector(".milieu_calendar");
 let cancel = document.getElementById("cancel");
 // const name = document.getElementById(name)
 
-const weekend = document.querySelector("#d6");
 
-
+let weekend = document.getElementById("d6");
 
 day.addEventListener("click", () => {
   popUp.style.display = "block";
+  
 });
 cancel.addEventListener('click', function(){
   popUp.style.display = "none"
+
 })
 
-//Local storage 
-const firstNameInput = document.querySelector("#firstName");
-const lastNameInput = document.querySelector("#lastName");
-const sexeInput = document.getElementById("sexe");
-const profileForm = document.querySelector("#profileForm");
-const profilesTbody = document.querySelector("#profilesTbody");
+// weekend.addEventListener("click" , function(){
+//   popUp.style.display = "none"
+// })
 
+form.addEventListener("submit" , (e) => {
+  e.preventDefault();
+  let name = document.getElementById("name").value;
+  let afficher = document.createElement('div');
+  afficher.innerHTML = `<span class= "affichage">${name}</span>`;
+  
+  
+})
 
+// formulaire.addEventListener('submit' ,function(e){
+//   e.preventDefault();
+//   
+//   
+//   
+//   console.log(afficher)
+// })
