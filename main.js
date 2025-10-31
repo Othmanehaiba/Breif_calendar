07d9e5c1892ca78e4de1b8083af1ca0be33aa90d
Othmane haiba <othmanehaiba4@gmail.com>
2025-10-31 09:34:02 +0100

    i fixed the probleme of cancling the form

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 const days_name = document.querySelectorAll(".days_name");
 const popUp = document.querySelector(".pop_up");
 const day = document.querySelector(".milieu_calendar");
-
+let cancel = document.getElementById("cancel");
 // const name = document.getElementById(name)
 
 const weekend = document.querySelector("#d6");
 
-weekend.addEventListener("click", () => {
-  popUp
-});
+
 
 day.addEventListener("click", () => {
   popUp.style.display = "block";
 });
+cancel.addEventListener('click', function(){
+  popUp.style.display = "none"
+})
 
-
+//Local storage 
+const firstNameInput = document.querySelector("#firstName");
+const lastNameInput = document.querySelector("#lastName");
+const sexeInput = document.getElementById("sexe");
+const profileForm = document.querySelector("#profileForm");
+const profilesTbody = document.querySelector("#profilesTbody");
 
 
